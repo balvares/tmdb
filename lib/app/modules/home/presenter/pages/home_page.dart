fa4f1804a58dda52359aeebe324ee253d218dddc
Bruna Alvares <balvares@agendese.io>
2024-04-02 14:47:57 -0300

    Componentizando drawer e criando grid principal

--- lib/app/modules/home/presenter/pages/home_page.dart
+++ lib/app/modules/home/presenter/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tmdb/shared/widgets/tmdb_label.dart';
 
 import '../../../../../shared/shared.dart';
 import '../../../../../shared/constants/keys.dart';
@@ -14,50 +13,55 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+  final List<String> items = [
+    'filme1',
+    'filme2',
+    'filme3',
+    'filme4',
+  ];
+
+  Widget _buildBody() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1 / 1.8,
+        ),
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return const Card(
+            child: Center(
+              child: Text(
+                'aaaaaaa', // Texto do item
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return TmdbAppBar(
+      key: const Key(homeAppBarKey),
+      title: const TmdbTitle(text: "Início"),
+      leading: IconButton(
+        icon: const Icon(Icons.menu, color: TmdbColors.primary),
+        onPressed: () => scaffoldKey.currentState!.openDrawer(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
+      appBar: _buildAppBar(),
+      drawer: const TmdbDrawer(),
       backgroundColor: TmdbColors.background,
-      drawer: Drawer(
-        shadowColor: Colors.transparent,
-        backgroundColor: TmdbColors.dark,
-        child: ListView(
-          padding: const EdgeInsets.only(
-            top: layoutSpace48,
-            left: layoutSpace16,
-            right: layoutSpace16,
-            bottom: layoutSpace48,
-          ),
-          children: const <Widget>[
-            TmdbTitle(
-              text: 'Olá, Bruna! 😀',
-              color: TmdbColors.secondary,
-              fontSize: layoutSpace24,
-            ),
-            SizedBox(height: layoutSpace16),
-            Divider(color: TmdbColors.primary),
-            SizedBox(height: layoutSpace16),
-            ListTile(
-              title: TmdbLabel(
-                text: "Filmes",
-                color: TmdbColors.secondary,
-              ),
-            ),
-          ],
-        ),
-      ),
-      appBar: TmdbAppBar(
-        key: const Key(homeAppBarKey),
-        title: const TmdbTitle(text: "Início"),
-        leading: IconButton(
-          onPressed: () {
-            scaffoldKey.currentState!.openDrawer();
-          },
-          icon: const Icon(Icons.menu, color: TmdbColors.primary),
-        ),
-      ),
+      body: _buildBody(),
     );
   }
 }
--- lib/shared/widgets/widgets.dart
+++ lib/shared/widgets/widgets.dart
@@ -1,4 +1,6 @@
+export 'tmdb_appbar.dart';
 export 'tmdb_button.dart';
+export 'tmdb_drawer.dart';
+export 'tmdb_label.dart';
 export 'tmdb_text_input.dart';
 export 'tmdb_title.dart';
-export 'tmdb_appbar.dart';
